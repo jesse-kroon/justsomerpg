f3142efeeea6950b35f9661b72e4eea2918c1b3e
Jesse Kroon <jessekroon2@gmail.com>
2024-05-14 23:13:17 +0200

    add some delay to the combat

--- combat.go
+++ combat.go
@@ -1,15 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func Combat(p *Player, e *Enemy) {
+	time.Sleep(time.Second * 3)
+
 	playerInitiative, EnemyInitiative := rollInitiative()
 	playerTurn := playerInitiative > EnemyInitiative
 
+	ClearScreen()
+	fmt.Printf("You are now entering combat with %s\n", e.name)
+	time.Sleep(time.Second * 1)
 	// Enter combat loop
 	// TODO: think of a way to add a basic hit/miss mechanic
 	for p.healthPoints != 0 && e.healthPoints != 0 {
 		if playerTurn {
+			time.Sleep(time.Millisecond * 500)
 			fmt.Printf("You deal %d damage to the enemy %s\n", p.weapon.Damage(), e.name)
 			e.healthPoints -= p.weapon.Damage()
 			if e.healthPoints <= 0 {
@@ -18,6 +27,7 @@ func Combat(p *Player, e *Enemy) {
 			}
 			playerTurn = false
 		} else {
+			time.Sleep(time.Millisecond * 500)
 			fmt.Printf("The enemy %s deals %d damage to you\n", e.name, e.weapon.Damage())
 			p.healthPoints -= e.weapon.Damage()
 			if p.healthPoints <= 0 {
@@ -28,7 +38,7 @@ func Combat(p *Player, e *Enemy) {
 	}
 }
 
-// Randomize this, for now player always starts
+// TODO: Randomize this, for now player always starts
 func rollInitiative() (int, int) {
 	return 2, 1
 }
